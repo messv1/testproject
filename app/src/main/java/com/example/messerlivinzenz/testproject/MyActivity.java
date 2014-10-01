85b1f67c0926fa2129b74ebeb35b3983f12ee8fa
messv1 <vmesserli@gmail.com>
2014-10-01 17:08:56 +0200

    added a comment for testing purpose

--- app/src/main/java/com/example/messerlivinzenz/testproject/MyActivity.java
+++ app/src/main/java/com/example/messerlivinzenz/testproject/MyActivity.java
@@ -16,6 +16,7 @@ public class MyActivity extends Activity {
 
 
     @Override
+    /*Added a comments for testing purpose*/
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
